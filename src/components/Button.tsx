947f063501eb6fe763dfadd9d58d677cd0fb8c30
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2025-11-20 23:27:35 +0400

    switch to tailwind

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,33 @@
-import classNames from "classnames";
-
 type ButtonProps = React.ComponentProps<"button"> & {
-  size?: "sm" | "lg";
-  variant?: "success" | "warning" | "danger";
+  size?: "sm" | "default" | "lg";
+  variant?: "default" | "success" | "warning" | "danger";
 };
 
 export default function Button({
   children,
-  className,
-  size,
-  variant,
+  size = "default",
+  variant = "default",
+  className = "",
   ...rest
 }: ButtonProps) {
-  const sizeClass = size && `button-${size}`;
-  const variantClass = variant && `button-${variant}`;
+  const sizeClasses = {
+    sm: "px-2.5 py-1 text-xs",
+    default: "px-3.5 py-1.5 text-sm",
+    lg: "px-5 py-2.5 text-base",
+  }[size];
 
-  const allClasses = classNames(sizeClass, className, variantClass);
+  const variantClasses = {
+    default: "bg-gray-200 text-gray-600 border border-gray-500 shadow-sm",
+    success: "bg-emerald-50 text-emerald-700 border border-emerald-700",
+    warning: "bg-amber-50 text-amber-400 border border-amber-400",
+    danger: "bg-red-50 text-red-400 border border-red-400",
+  }[variant];
+
   return (
-    <button className={allClasses} {...rest}>
+    <button
+      {...rest}
+      className={`${sizeClasses} ${variantClasses} rounded font-semibold ${className}`}
+    >
       {children}
     </button>
   );
